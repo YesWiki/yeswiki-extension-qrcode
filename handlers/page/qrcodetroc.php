3595bc0c632ecf5fcd02afc83e294762fa002fda
Florian Schmitt <mrflos@gmail.com>
2021-08-13 09:36:11 +0300

    fix(qrcode): no error repporting, better js integration

--- handlers/page/qrcodetroc.php
+++ handlers/page/qrcodetroc.php
@@ -1,5 +1,17 @@
 <?php
 
+// 2019 : 10 inscrits, 15 liens
+// 2017 : 5 inscrits, 7 liens
+if (empty($_GET['annuaire'])) {
+  $annuaire = '10';
+} else {
+  $annuaire = $_GET['annuaire'];
+}
+if (empty($_GET['liens'])) {
+  $liens = '15';
+} else {
+  $liens = $_GET['liens'];
+}
 
 $output = '';
 // on recupere les entetes html mais pas ce qu'il y a dans le body
@@ -40,14 +52,13 @@ $js = "
 
   function getUtilisateurs()
   {
-  	$.getJSON('wakka.php?wiki=Bazar/json&demand=entries&form=1', function(dataUtilisateurs) {
+  	$.getJSON('wakka.php?wiki=Bazar/json&demand=entries&form=".$annuaire."', function(dataUtilisateurs) {
       tabUsers = dataUtilisateurs;
       var data = Object.values(dataUtilisateurs);
       for (var i=0; i<data.length; i++) {
         tabUtilisateurs[i]=data[i]['bf_titre']+' '+data[i]['bf_nom'];
       }
   	}).then(function() {
-      console.log(tabUtilisateurs);
       getLiens();
     });
   }
@@ -55,13 +66,13 @@ $js = "
 
   function getLiens()
   {
-  	$.getJSON('wakka.php?wiki=Bazar/json&demand=entries&form=2', function(dataLiens) {
+  	$.getJSON('wakka.php?wiki=Bazar/json&demand=entries&form=".$liens."', function(dataLiens) {
       var data = Object.values(dataLiens);
       for (var i=0; i<data.length; i++) {
         tabLiens[i] = new Array();
-        var u1 = data[i]['listefiche5part1'];
+        var u1 = data[i]['listefiche".$annuaire."part1'];
         tabLiens[i][0]=tabUsers[u1]['bf_titre']+' '+tabUsers[u1]['bf_nom'];
-        var u2 = data[i]['listefiche5part2'];
+        var u2 = data[i]['listefiche".$annuaire."part2'];
         tabLiens[i][1]=tabUsers[u2]['bf_titre']+' '+tabUsers[u2]['bf_nom'];
       }
   	});
