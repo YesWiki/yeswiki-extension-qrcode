a6f35daaabfda47898b7a9de31c4decf848fbb0c
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2021-09-06 09:33:55 +0200

    fix(qrcodescan.js): fps 10 to 20

--- javascripts/qrcodescan.js
+++ javascripts/qrcodescan.js
@@ -222,7 +222,7 @@ var lastResult,
 
 // This method will trigger user permissions
 const html5QrCode = new Html5Qrcode(/* element id */ "qrreader", { formatsToSupport: [ Html5QrcodeSupportedFormats.QR_CODE ] })
-const config = { fps: 10, qrbox: 250 }
+const config = { fps: 20, qrbox: 250 }
 const qrCodeSuccessCallback = (decodedText, decodedResult) => {
   //console.info(decodedText, decodedResult)
   if (decodedText !== lastResult) {
